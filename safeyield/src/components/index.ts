a8d9475976e3c576373bf7dcb61781bfc5e7fbee
San Clemente <40546417+san-npm@users.noreply.github.com>
2026-01-14 22:42:07 +0100

    Delete safeyield directory

--- safeyield/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { Footer } from './Footer';
-export { TopPools, TopPoolCard } from './TopPools';
-export { PoolsTable } from './PoolsTable';
-export { Filters } from './Filters';
-export { Stats } from './Stats';
-export { SecurityScore, SecurityBadge } from './SecurityScore';
